529a7096bd9d041b132baf906d2232423b19f13e
arctheowl <ldevlin95@hotmail.co.uk>
2020-09-09 23:43:48 +0100

    update

--- deck/card.go
+++
@@ -1,7 +0,0 @@
-package deck
-
-// Card struct is a represenation of a playing card, a value will be an int and the suit is a single letter string to represent the 4 suited deck
-type Card struct {
-	Value int
-	Suit  string
-}
